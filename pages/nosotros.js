146c0fe980df528f68ec0ac03615a4ab35984386
Hazael Vite <14224891+HazaelVite@users.noreply.github.com>
2022-07-12 19:42:10 -0500

    Creando la pagina de nosotros

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,12 +1,22 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import { Layout } from '../components/Layout'
+import styles from '../styles/Nosotros.module.css'
 
 const Nosotros = () => {
   return (
     <div>
       <Layout pagina='Sobre Nosotros'>
-        <h1>Desde Nosotros</h1>
-        <Link href='/'>Inicio</Link>
+        <main className='contenedor'>
+          <h2 className='heading'>Nosotros</h2>
+          <div className={styles.contenido}>
+            <Image layout="responsive" width={600} height={450} src="/img/nosotros.jpg" alt='Sobre Nosotros'/>
+            <div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tempus mattis turpis, in porta lorem facilisis id. Vestibulum et urna vehicula, maximus felis nec, pharetra nunc. In laoreet ex quis felis venenatis, sed eleifend turpis imperdiet. Cras nisi risus, convallis a lorem quis, fermentum sollicitudin dolor.</p>
+              <p>Donec in gravida leo, a ullamcorper ligula. Mauris fringilla pretium neque nec pellentesque. Aenean dictum aliquam rutrum. Proin in neque a sem hendrerit iaculis sed sed tellus. Maecenas metus enim, volutpat et quam sit amet, varius pharetra massa. Integer a tellus sed lorem dapibus egestas sit amet a urna.</p>
+            </div>
+          </div>
+        </main>
       </Layout>
     </div>
   )
